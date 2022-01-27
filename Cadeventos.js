93ad26b29153ea7ad68af37083dc9f7a4176f0c8
Luciano Souza <goluci90@gmail.com>
2022-01-27 13:38:57 -0300

    Criação do arquivo cadeventos

---
+++ Cadeventos.js
@@ -0,0 +1,22 @@
+let numeroAlunos = 10;
+
+for(let contador = 0; contador < numeroAlunos; contador++) {
+    //console.log(contador);
+    // Resto -> Retorna o resto de uma divisão inteira
+    //if(contador % 2 == 0) { 
+        //console.log(`o numero ${contador} é PAR`)
+//}
+    // Resto % -> Retorna o resto de uma divisão inteira
+    
+    if(contador == 0) {
+        console.log("O numero é ZERO")
+    }else if(contador % 2 == 0) { 
+        // Concatenação 
+        console.log(`O numero ` + contador + ` é PAR`)
+    } else if(contador % 2 == 1) {
+        //Interpolação
+        console.log(`O numero ${contador} é IMPAR`)
+    
+    }
+
+}
